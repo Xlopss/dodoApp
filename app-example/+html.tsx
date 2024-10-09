c53ee083f27f9dbc2b6df474ad30a3c338e0a47d
Xlopss <estelle.alleau@gmail.com>
2024-10-09 20:34:00 +0200

    Revert "Initial commit"
    This reverts commit 9353e750fc1b4b1ef7cc2d4a7935b73e91582451.

--- app-example/+html.tsx
+++ app-example/+html.tsx
@@ -21,8 +21,7 @@ export default function Root({ children }: PropsWithChildren) {
 
         {/* Using raw CSS styles as an escape-hatch to ensure the background color never flickers in dark-mode. */}
         <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
-          <title></title>
-          {/* Add any additional <head> elements that you want globally available on web... */}
+        {/* Add any additional <head> elements that you want globally available on web... */}
       </head>
       <body>{children}</body>
     </html>
