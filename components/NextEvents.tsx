15facdb803b07515cb58fe8b8e662085cd50d8d6
Xlopss <estelle.alleau@gmail.com>
2024-10-19 17:10:00 +0200

    NextOne -> NextEvents.tsx

--- components/NextEvents.tsx
+++ components/NextEvents.tsx
@@ -5,9 +5,27 @@ export default function NextEvents() {
 
     // @ts-ignore
     //TODO: Tej te TS-ignore
-const sortedEvents = events.filter(event => event.dodoleft >= 0).sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+    const sortedEvents = events.filter(event => event.dodoleft >= 0).sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+    const nextEvent = events.filter(event => event.dodoleft >= 0).reduce((prev, curr) => {
+        return new Date(prev.date) < new Date(curr.date) ? prev : curr;
+    });
+
     return (
         <View className={'mocha bg-ctp-base'} style={{flex: 1, padding: 16, width: '100%'}}>
+
+            <View style={{margin: 8}}>
+                <Text className={'text-ctp-lavender text-2xl'}>Bientôt :</Text>
+            </View>
+
+            <View style={{borderWidth: 1, borderColor: '#b4befe', padding: 20, borderRadius: 8, marginBottom: 50}}>
+                <Text className={'text-ctp-lavender text-5xl'}>{nextEvent.title}</Text>
+                <View style={{flexDirection: "row", justifyContent: 'space-between', alignItems: 'center'}}>
+                    <Text className={'text-ctp-lavender text-xl'}>DANS</Text>
+                    <Text
+                        className={'text-ctp-lavender text-5xl'}>{nextEvent.dodoleft} {nextEvent.dodoleft < 2 ? 'dodo' : 'dodos'}</Text>
+                </View>
+
+            </View>
 
             <View
                 style={{
@@ -17,11 +35,11 @@ const sortedEvents = events.filter(event => event.dodoleft >= 0).sort((a, b) => 
                     marginBottom: 16,
                     marginRight: 5
                 }}>
-                <Text className={'font-bold text-ctp-lavender text-2xl'}>Événements à venir</Text>
+                <Text className={'font-bold text-ctp-lavender text-3xl'}>Événements à venir</Text>
                 <TouchableOpacity onPress={() => alert('Calendrier touché !')}>
                     <Image
                         source={require('../assets/img/calendar.png')}
-                        style={{width: 30, height: 30, tintColor: '#b4befe'}}/>
+                        style={{width: 35, height: 35, tintColor: '#b4befe'}}/>
                 </TouchableOpacity>
             </View>
 
@@ -36,7 +54,8 @@ const sortedEvents = events.filter(event => event.dodoleft >= 0).sort((a, b) => 
                         borderBottomWidth: 1,
                         borderBottomColor: '#ccc'
                     }}>
-                        <Text className={'text-ctp-lavender text-xl'}>{item.title}</Text>
+                        <Text
+                            className={'text-ctp-lavender text-xl'}>{item.title.length > 25 ? item.title.substring(0, 25) + '...' : item.title}</Text>
                         <View style={{
                             padding: 5,
                             backgroundColor: '#b4befe',
